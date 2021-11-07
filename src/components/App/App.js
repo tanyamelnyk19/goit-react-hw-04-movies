46472cb059da142d2cb9a317acadf673df223eb5
tanyamelnyk19 <79224689+tanyamelnyk19@users.noreply.github.com>
2021-11-07 17:57:39 +0200

    add start files

---
+++ src/components/App/App.js
@@ -0,0 +1,14 @@
+// import { Route } from 'react-router-dom';
+import './App.css';
+import Navigation from '../Navigation/Navigation';
+
+function App() {
+  return (
+    <div className="App">
+      <Navigation />
+      {/* <Route> */}
+    </div>
+  );
+}
+
+export default App;
---
+++ src/services/moviesApi.js
@@ -0,0 +1,13 @@
+// import axios from 'axios';
+
+// const API_KEY = '4444c186d8016dd337b1527a507fbda9';
+
+// axios.defaults.baseURL =
+//   'https://pixabay.com/api/?image_type=photo&orientation=horizontal';
+
+// export default async function getMovies(query, page) {
+//   const {
+//     data: { hits },
+//   } = await axios.get(`&q=${query}&page=${page}&per_page=12&key=${API_KEY}`);
+//   return hits;
+// }
